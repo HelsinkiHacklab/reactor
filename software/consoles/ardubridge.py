14197a99f05e1a0c9b3ca187e41129cb3452c57a
inZka <tapio@laatikko.local>
2011-10-08 10:46:43 +0300

    added real value to servo range interpolator, and started to simplify this to be only signal wrapper generator
    also realized that this will be only signal wrapper. most arduino
    commands should be in ardubus.py and matching sketch.

--- software/consoles/ardubridge.py
+++ software/consoles/ardubridge.py
@@ -11,6 +11,11 @@ import ConfigParser
 
 import ardubus
 
+#NOTE: this will be just signal wrapper generator layer between lower ardubus and 
+#game components
+#this could be seperate entinty and just relay upper level signals to lower level signals
+#instead of being derivate and calling methods directly, but that might be little overkill 
+
 class ardubus_console(ardubus.ardubus):
     def __init__(self, bus, config):
 	ardubus.ardubus.__init__(self, bus, config.get('board', 'name'), config)
@@ -24,6 +29,10 @@ class ardubus_console(ardubus.ardubus):
             if len(input_act) > 1:
                 receiver = self.call_with_offset(int(input_act[1]), 
                                                  self.__getattribute__(input_act[0]))                
+                if len(input_act) >=3:
+                    receiver = self.make_interpolate(int(input_act[2]), 
+                                                     int(input_act[3]), 
+                                                     receiver)
             else:
                 receiver = self.__getattribute__(input_act[0])
             
@@ -32,33 +41,52 @@ class ardubus_console(ardubus.ardubus):
 
         for output in config.items('outputs'):
             print "setting signal", output[0], "for command", output[1]
-            self.__dict__[input[0]] = dbus.service.signal('fi.hacklab.ardubus',
-                                       self.__getattribute__(input[1]))
-            
-    def message_received(self, input_buffer):
-        print "received message from serial: %s" %repr(input_buffer)
-
-    #NOTE: we have to keep always id / item number as first parameter 
+            #TODO: mapping user inputs from arduino into signals
+            #self.__dict__[output[0]] = dbus.service.signal('fi.hacklab.ardubus')(self.tmp)            
+            #print "%s" % repr(self.__dict__[output[0]])
+            #self.__dict__[output[0]]()
+    
     def call_with_offset(self, offset, method):
         def _m(num, *args):
             method(num + offset, *args)
         return _m
 
-    #examples of signals and handlers 
-    
+    def make_interpolate(self, vmin, vmax, method):
+        scale = float(180) / float(vmax - vmin)
+        def _m(index, value, *args):
+            method(index, int( (value - vmin)*scale), *args)
+        return _m
+  
+    #override default signal generators with handlers  
+    def dio_change(self, pin, state, sender):
+        #self.dios[pin](state) #call singal method for this pin change 
+        #print "SIGNALLING: Pin %d changed to %d on %s" % (pin, state, sender)
+        pass
+
+    def dio_report(self, pin, state, time, sender):
+        #print "SIGNALLING: Pin %d has been %d for %dms on %s" % (pin, state, time, sender)
+        pass
+  
+    def aio_change(self, pin, value, sender):
+        #print "SIGNALLING: Pin %d has been %d for %dms on %s" % (pin, state, time, sender)
+         pass
 
     #examples of mappers called from signals 
     def dbg_receive_signal(self, *args, **kwargs):
         print "Got args: %s" % repr(args)
         print "Got kwargs %s" % repr(kwargs)
-
+    
+    #these will be deprecated, methods that communicate with arduino sketch
+    #from base class will be used
     def gauge_display(self, gnumber, value):
+        print "deprecated"
         #command servo gauge to display value here 
-        self.send_serial_command("SG%d:%d" % (gnumber, value))
+        #self.send_serial_command("SG%d:%d" % (gnumber, value))
 
     def set_led(self, lnumber, state):
+        print "deprecated"
         #command led to state on,off,flicker here 
-        self.send_serial_command("SL%d:%d" % (lnumber, state))
+        #self.send_serial_command("SL%d:%d" % (lnumber, state))
 
 # these might be optional
 # simulator could always send all signal states 
