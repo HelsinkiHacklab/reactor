ddb2eaa5fb1142c4012d86e654dd217c65086762
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-10-16 00:16:36 +0300

    JBOLeds variant of the RGB baord, when we just want to control 3x16 leds individually

---
+++ software/arDuBUS/sketch/libraries/pca9635RGBJBOL/pca9635RGBJBOL.h
@@ -0,0 +1,19 @@
+// safety againts double-include
+#ifndef pca9635RGB_h
+#define pca9635RGB_h
+#include <WProgram.h> 
+#include <pca9635RGB.h>
+
+class pca9635RGBJBOL : public pca9635RGB
+{
+    public:
+        // Proxies to all the individual drivers
+        boolean set_led_pwm(byte ledno, byte cycle);
+        boolean set_led_mode(byte ledno, byte mode);
+        boolean set_led_mode(byte mode); 
+        boolean set_driver_mode(byte mode);
+        boolean set_sleep(byte sleep);
+};
+
+#endif
+// *********** END OF CODE **********
