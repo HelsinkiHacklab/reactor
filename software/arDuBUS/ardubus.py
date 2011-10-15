beac2c1c1988b542830ea7b3e40ead196531ec14
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-10-16 01:05:28 +0300

    use separate launcher script, this allows precompilation of the module itself for faster launch times

--- software/arDuBUS/ardubus.py
+++ software/arDuBUS/ardubus.py
@@ -152,26 +152,5 @@ class ardubus(dbus.service.Object):
 
 
 if __name__ == '__main__':
-    import dbus.mainloop.glib, gobject
-    gobject.threads_init()
-    dbus.mainloop.glib.threads_init()
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    import ConfigParser
-    config = ConfigParser.SafeConfigParser()
-    if not os.path.isfile('ardubus.conf'):
-        config.add_section('arduino0')
-        config.set('arduino0', 'device', '/dev/ttyUSB0')
-        # TODO: Other defaults
-        with open('ardubus.conf', 'wb') as configfile:
-            config.write(configfile)
-    config.read('ardubus.conf')
-
-    bus = dbus.SessionBus()
-
-    service_objects = {}
-    for board_name in config.sections():
-        service_objects[board_name] = ardubus(bus, board_name, config)
-
-    loop = gobject.MainLoop()
-    loop.run()
+    print "Use ardbus_launcher.py"
+    sys.exit(1)
---
+++ software/arDuBUS/ardubus_launcher.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os,sys
+import dbus
+import dbus.service
+from ardubus import ardubus 
+
+if __name__ == '__main__':
+    import dbus.mainloop.glib, gobject
+    gobject.threads_init()
+    dbus.mainloop.glib.threads_init()
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    import ConfigParser
+    config = ConfigParser.SafeConfigParser()
+    if not os.path.isfile('ardubus.conf'):
+        config.add_section('arduino0')
+        config.set('arduino0', 'device', '/dev/ttyUSB0')
+        # TODO: Other defaults
+        with open('ardubus.conf', 'wb') as configfile:
+            config.write(configfile)
+    config.read('ardubus.conf')
+
+    bus = dbus.SessionBus()
+
+    service_objects = {}
+    for board_name in config.sections():
+        service_objects[board_name] = ardubus(bus, board_name, config)
+
+    # TODO: Add some nicer way to exit than ctrl-c
+    loop = gobject.MainLoop()
+    loop.run()
