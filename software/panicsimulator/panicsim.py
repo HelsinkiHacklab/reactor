52534abdc02d8cc15120ad682500d86d3c9e38b9
Hans Häggström <zzorn@iki.fi>
2011-10-22 21:20:44 +0300

    Panicsim

--- software/panicsimulator/panicsim.py
+++ software/panicsimulator/panicsim.py
@@ -10,8 +10,21 @@ import dbus.service
 import dbus.mainloop.glib
 
 from collections import namedtuple
+import threading
 
-fuel_channel = namedtuple('temp flux rodpos pressure fluxout outtemp cooling rodup roddown rodstepped')
+class fuel_channel():
+    def __init__(self, temp, flux, rodpos, pressure, fluxout, outtemp, 
+                 cooling, rodup, roddown, rodstepped):
+                 self.temp = temp
+                 self.flux = flux
+                 self.rodpos = rodpos
+                 self.pressure = pressure
+                 self.fluxout = fluxout
+                 self.outtemp = outtemp
+                 self.cooling = cooling
+                 self.rodup = rodup
+                 self.roddown = roddown
+                 self.rodstepped = rodstepped
 
 fluxSpreadFactor = 0.01
 tempSpreadFactor = 0.01
@@ -21,13 +34,22 @@ coolingEffectOnTemp = 0.01
 normalCooling = 1.0
 rodMoveSpeed = 0.5
 coolingStompIncrease = 10
+fluxDissipation = 0.01
+coolDownSpeed = 1
 grid_w = 7
 grid_h = 7
+explodeTemp = 1000
+recoverTemp = 500
 
-class ardubus(dbus.service.Object):
+highestTemp = 0
+exploding = False
+
+class reactor(dbus.service.Object):
+
+
     def __init__(self, bus):
         self.object_name = 'reactor'
-        self.object_path = '/fi/hacklab/ardubus/' + object_name
+        self.object_path = '/fi/hacklab/ardubus/' + self.object_name
         self.bus_name = dbus.service.BusName('fi.hacklab.ardubus', bus=bus)
         dbus.service.Object.__init__(self, self.bus_name, self.object_path)
         self.bus = bus
@@ -35,7 +57,7 @@ class ardubus(dbus.service.Object):
         # We can either listen all signals on the interface regardles of the board or specify we want to listen only to specific board
         self.bus.add_signal_receiver(self.controlRodUp, dbus_interface = "fi.hacklab.ardubus", signal_name = "control_rod_up")
         self.bus.add_signal_receiver(self.controlRodDown, dbus_interface = "fi.hacklab.ardubus", signal_name = "control_rod_down")
-         self.bus.add_signal_receiver(self.controlRodStepped, dbus_interface = "fi.hacklab.ardubus", signal_name = "control_rod_stepped")       
+        self.bus.add_signal_receiver(self.controlRodStepped, dbus_interface = "fi.hacklab.ardubus", signal_name = "control_rod_stepped")       
          
         self.fuel_channels = []
         self.ph_channels = [None, None,    0,   1,    2, None, None, 
@@ -46,9 +68,13 @@ class ardubus(dbus.service.Object):
                             None,    26,  27,  28, None,   29, None,
                             None,  None,  30,  31,   32, None, None]
         
-        for i in range(0, 32):
-            self.fuel_channels.append(fuel_channel(200, 100, 50, 100, 100, 100, 100))
-    
+        for i in range(0, 33):
+            self.fuel_channels.append(fuel_channel(200, 100, 50, 100, 100, 100, 100, False, False, False))
+        
+        self.simulator_thread = threading.Thread(target=self.simulator_loop)
+        self.simulator_thread.setDaemon(1)
+        self.simulator_thread.start()
+
     def get_neighbours(self, index):
         neighbours = []
         for x in range(-1, 1):
@@ -77,12 +103,23 @@ class ardubus(dbus.service.Object):
     @dbus.service.signal('fi.hacklab.ardubus')
     def control_rod_pos(self, index, value):
         pass 
-               
+        
+    def simulator_loop(self):
+        while True:
+            self.simulate_step(1)
+                           
     def simulate_step(self, time):
+        global exploding
+        global highestTemp
+        highestTemp = 0
         for c in self.fuel_channels:
+            #print type(c), c, c.temp
+            highestTemp = max(highestTemp, c.temp)
+            c.temp = 6
             c.temp += c.flux * fluxHeatGeneration * time
             c.temp -= c.cooling * coolingEffectOnTemp * time 
             c.flux += (c.rodpos - 50) * rodEffectOnFlux * time
+            c.flux *= (1.0 - fluxDissipation)
             
             if c.rodup:
                 c.rodPos += rodMoveSpeed * time
@@ -97,7 +134,7 @@ class ardubus(dbus.service.Object):
                 c.cooling += coolingStompIncrease
                 c.rodstepped = False
             if (c.cooling > normalCooling):
-                c.cooling -= cooldownSpeed * time
+                c.cooling -= coolDownSpeed * time
             
             c.outflux = c.flux
             c.outtemp = c.temp
@@ -107,16 +144,25 @@ class ardubus(dbus.service.Object):
         #for c in self.fuel_channels:
         #    c.flux += # Sum of adjacent channel outfluxes multiplied by fluxSpreadFactor * time
         #    c.temp += # Sum of adjacent channel outtemps multiplied by tempSpreadFactor * time
-        for i, i2 in enumerate(self.ph_channels):
-            if i2:
-                item = self.get_neighbours(i)
-                i2.flux = sum( [i.flux for i in item] ) / len(item)
+        for i, currentRod in enumerate(self.ph_channels):
+            if currentRod !=None:
+                #print i, currentRod
+                currentRod = self.fuel_channels[currentRod]
+                neighbors = self.get_neighbours(i)
+                currentRod.flux = sum( [neigborRod.flux for neigborRod in neighbors] ) * fluxSpreadFactor * time 
+                currentRod.temp = sum( [neigborRod.temp for neigborRod in neighbors] ) * tempSpreadFactor * time
                 
         for i, c in enumerate(self.fuel_channels):
             self.control_rod_pos(i, c.rodpos)
 
         # TODO: For the four measurement rods, send out flux and temperature readings.       
         
+        if ((highestTemp > explodeTemp) and not exploding):
+            # TODO: Play explode sound
+            exploding = True
+        if ((highestTemp < recoverTemp) and exploding):
+            exploding = False
+        print "Maxtemp ", highestTemp     
 
 if __name__ == '__main__':
     gobject.threads_init()
@@ -127,6 +173,7 @@ if __name__ == '__main__':
     bus = dbus.SessionBus()
     recactor = reactor(bus)
 
+    
     loop = gobject.MainLoop()
     loop.run()
 
