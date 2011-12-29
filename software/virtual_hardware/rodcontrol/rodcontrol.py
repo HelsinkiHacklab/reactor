0b09af16f10747364d58d12d052dbb106e80d95b
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-12-29 02:26:23 +0200

    boilerplate and example grid core

---
+++ software/virtual_hardware/rodcontrol/rodcontrol.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Rod control panel"""
+
+import sys
+
+from PySide import QtCore
+from PySide import QtGui
+from PySide import QtDeclarative
+
+
+
+
+app = QtGui.QApplication(sys.argv)
+
+view = QtDeclarative.QDeclarativeView()
+view.setWindowTitle(__doc__)
+view.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
+
+rc = view.rootContext()
+
+#rc.setContextProperty('controller', controller)
+#rc.setContextProperty('pythonListModel', zenItemList)
+view.setSource(__file__.replace('.py', '.qml'))
+
+view.show()
+app.exec_()
+
