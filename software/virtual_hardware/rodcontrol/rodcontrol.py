ded303f7a855fde500a624923c14c7e82f8e4bc7
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-12-30 21:21:30 +0200

    randomly move all servos

--- software/virtual_hardware/rodcontrol/rodcontrol.py
+++ software/virtual_hardware/rodcontrol/rodcontrol.py
@@ -61,8 +61,7 @@ class Proxy(QtCore.QObject):
 
     def update_gauge(self):
         import random
-        self.qml_root.findChild(QtDeclarative.QDeclarativeItem, "servo1").setUSec(random.randint(1000,2000))
-        #self.qml_root.findChild(QtDeclarative.QDeclarativeItem, "servo1").setPosition(random.randint(0,255))
+        self.qml_root.findChild(QtDeclarative.QDeclarativeItem, "servo%d"%random.randint(0,31)).setUSec(random.randint(1000,2000))
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
