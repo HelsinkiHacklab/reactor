32f1af1be8ae9a5dc57135faec829225fcb76fd9
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-04-01 01:46:58 +0300

    rods drive servos, the map is incorrect but it works

--- software/middleware/middleware.py
+++ software/middleware/middleware.py
@@ -34,10 +34,12 @@ gauges8leds_map = [[' ', ' ', '*', '*', '*', ' ', ' '],
 max_temp = 800
 
 
+reactor_square_side = 7
+
 class middleware(service.baseclass):
     def __init__(self, config, launcher_instance, **kwargs):
         super(middleware, self).__init__(config, launcher_instance, **kwargs)
-
+        self.config_reloaded()
 
         self.load_nm()
         self.ardu_rodservos = None
@@ -51,20 +53,39 @@ class middleware(service.baseclass):
 
         self.bus.add_signal_receiver(self.blowout, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_blowout")
 
-        # there is some bug here and I wish to debug stomping now
-        #self.bus.add_signal_receiver(self.depth_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_depth")
+        self.bus.add_signal_receiver(self.depth_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_depth")
 
         self.max_neutrons_seen = 0
-        # Listen temp/neutrons only from the measurment wells
-        #self.bus.add_signal_receiver(self.neutron_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_neutrons")
-        #self.bus.add_signal_receiver(self.temp_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_temp",)
+        
+        # Listen temp/neutrons only from the measurment wells (filtering done on the receiver end it seems we could not do wildcard path matching afterall [maybe I should rethink these interface spaces])
+        self.bus.add_signal_receiver(self.neutron_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_neutrons")
+        self.bus.add_signal_receiver(self.temp_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_temp",)
         
         #self.bus.add_signal_receiver(self.temp_report, dbus_interface = 'fi.hacklab.reactorsimulator.engine', signal_name = "emit_temp",)
 
         self.bus.add_signal_receiver(self.stomp_received, dbus_interface = "fi.hacklab.ardubus", signal_name = "dio_change", path="/fi/hacklab/ardubus/arduino2")
 
+
+    def config_reloaded(self):
+        # Transpose the rod servo map to something more usable
+        self.rod_servo_map = [[None for i in range(reactor_square_side)] for i in  range(reactor_square_side)]
+        for board in self.config['rod_servo_map'].keys():
+            for servo_idx in self.config['rod_servo_map'][board].keys():
+                rodx,rody = self.config['rod_servo_map'][board][servo_idx]
+                self.rod_servo_map[rodx][rody] = (servo_idx, board)
+
+
+    def depth_report(self, x, y, depth, *args):
+        servo_position = int(np.interp(float(depth), [-2,reactor_square_side],[0,255]))
+        #print "depth report for rod %d,%d, depth is %.3f corresponding to servo position %d" % (x,y,depth, servo_position)
+            
+        servo_idx,board_name = self.rod_servo_map[int(x)][int(y)]
+        
+        # TODO: We should cache these objects while keeping calling conventions this simple (with automatic try/catch fallback for changed numeric id)
+        self.bus.get_object('fi.hacklab.ardubus', "/fi/hacklab/ardubus/%s" % board_name).set_servo(dbus.Byte(servo_idx), dbus.Byte(servo_position))
+
     def stomp_received(self, pin, state, sender, *args):
-        print "Pin %d changed(index) to %s on %s" % (pin, repr(state), sender)
+        #print "Pin %d changed(index) to %s on %s" % (pin, repr(state), sender)
         if bool(state):
             # high means pulled up, ie not switched
             return
@@ -118,9 +139,6 @@ class middleware(service.baseclass):
         # TODO: make these configurable
         self.nm.play_sample('steam_release.wav')
 
-    def depth_report(self, x, y, depth, *args):
-        rod_id = int(rod_map[x][y]) -1 # forgot to start from zero
-        self.set_rod_servo(rod_id, int((255.0/9)*(depth+2)))
 
     def load_nm(self):
         self.nm = self.bus.get_object('fi.hacklab.noisemaker', '/fi/hacklab/noisemaker')
@@ -136,23 +154,24 @@ class middleware(service.baseclass):
     	self.set_rod_leds = self.ardu_rodleds.get_dbus_method('set_jbol_pwm', 'fi.hacklab.ardubus')
 
     def neutron_report(self, x, y, neutrons, *args):
-        #print "neutron check %s" % rod_map[x][y]
-        if (rod_map[x][y] <> '#'):
+        if self.rod_servo_map[int(x)][int(y)]:
+            # This is an actual rod (the measurement wells have "None" here
             return
+
         # Autoscale at least until we know what the scales are
         print "neutrons for %d,%d,%s" % (x,y,neutrons)
         current_max = max(neutrons)
         if current_max > self.max_neutrons_seen:
             self.max_neutrons_seen = current_max
-        led_base_index = (int(gauges8leds_map[x][y])*8)+4
+        led_base_index = (int(gauges8leds_map[x][y])*8)+1
         neutron_avg = sum(neutrons)/len(neutrons)
         self.led_gauge(led_base_index, 4, neutron_avg, self.max_neutrons_seen)
 
     def temp_report(self, x, y, temps, *args):
-        #print "temp check %s" % rod_map[x][y]
-        if (rod_map[x][y] <> '#'):
+        if self.rod_servo_map[int(x)][int(y)]:
+            # This is an actual rod (the measurement wells have "None" here
             return
-        print "temperatures for %d,%d,%s" % (x,y,neutrons)
+        print "temperatures for %d,%d,%s" % (x,y,temps)
         led_base_index = (int(gauges8leds_map[x][y])*8)
         temp_avg = sum(temps)/len(temps)
         self.led_gauge(led_base_index, 4, temp_avg, self.max_temp)
