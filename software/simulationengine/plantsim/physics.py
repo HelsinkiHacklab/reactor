57bd4a47ae309976944bcc07673a2133e4002f2c
Hacklab-olohuonelappari <Hacklab-olohuonelappari>
2012-04-01 13:34:33 +0300

    Testcommit.

--- software/simulationengine/plantsim/physics.py
+++ software/simulationengine/plantsim/physics.py
@@ -12,6 +12,7 @@ medium_pipe_size_m2 = 0.5
 # Gravitational force
 earth_g = 9.81
 
+
 # Material properties
 def water_density(temperature_C):
     # TODO: Water density at 100 degrees is about 950, and at 300 degrees about 500, so we may need an approximate function
