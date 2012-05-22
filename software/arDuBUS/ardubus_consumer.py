e0f0f21278b773adf3eefcec221a1d278f43f831
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-22 22:56:11 +0300

    fix sleep-deprivation induced stupidity

--- software/arDuBUS/ardubus_consumer.py
+++ software/arDuBUS/ardubus_consumer.py
@@ -5,4 +5,7 @@ bus = dbus.SessionBus()
 launcher = bus.get_object('fi.hacklab.ardubus.launcher', '/fi/hacklab/ardubus/launcher')
 print launcher.list_boards()
 
+def get_board(bname):
+    return bus.get_object("fi.hacklab.ardubus.%s" % bname, "/fi/hacklab/ardubus/%s" % bname)
+
 p = bus.get_object('fi.hacklab.ardubus.launcher', '/fi/hacklab/ardubus/aliases_test_board')
