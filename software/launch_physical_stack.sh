a755e6422f76d8c6885bc1106fff6439a4917c25
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-23 17:47:43 +0300

    untested code to launch the stack with physical consoles

---
+++ software/launch_physical_stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+TPROG="gnome-terminal -e "
+# The visualizer does not have any use for a terminal
+python simulationengine/visualizer_launcher.py &
+# For others it's usefull for debugging
+$TPROG "python simulationengine/simulation_launcher.py" &
+$TPROG "python noisemaker/noisemaker_launcher.py" &
+$TPROG "python arDuBUS/ardubus_launcher.py" &
+sleep 2 # Give the others time to actually register on the bus before firing up the middleware that will throw a hissy fit if it doesn't seem them
+$TPROG "python middleware/middleware_launcher.py" &
