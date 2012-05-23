f6fe654dfae40d43ee1407c5cb00088da258d063
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-23 17:56:46 +0300

    window titles and since we depend already on gnome-termninal specific feature do not bother configureing the terminal prog

--- software/launch_physical_stack.sh
+++ software/launch_physical_stack.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -ex
-TPROG="gnome-terminal -e "
 # The visualizer does not have any use for a terminal
 python simulationengine/visualizer_launcher.py &
 # For others it's usefull for debugging
-$TPROG "python arDuBUS/ardubus_launcher.py" &
-$TPROG "python noisemaker/noisemaker_launcher.py" &
-$TPROG "python simulationengine/simulation_launcher.py" &
+gnome-terminal --title="arDuBUS" -e "python arDuBUS/ardubus_launcher.py" &
+gnome-terminal --title="Noisemaker" -e "python noisemaker/noisemaker_launcher.py" &
+gnome-terminal --title="Simulationengine" -e "python simulationengine/simulation_launcher.py" &
 sleep 5 # Give the others time to actually register on the bus before firing up the middleware that will throw a hissy fit if it doesn't seem them
-$TPROG "python middleware/middleware_launcher.py" &
+gnome-terminal --title="Middleware" -e "python middleware/middleware_launcher.py" &
