5ba1bf2d59a9a16cc74a6a0354472e356b1678c0
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-06-06 10:45:43 +0300

    we depend on realpth now

--- software/install_dependencies.sh
+++ software/install_dependencies.sh
@@ -1,5 +1,8 @@
-#! /bin/bash
+#!/bin/bash -x
 
 # Python and python libs
 sudo apt-get install python python-pygame python-yaml python-dbus python-gobject python-pyside python-gst0.10
+# Realpath required for calling the launchers via desktop links
+sudo apt-get install realpath
+
 
