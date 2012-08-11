a9a32daa8da4dc727364657bef409ee74c99e2b0
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-08-11 19:31:35 +0300

    added some notes about pin numbers, new boot code to indicate with a led when we are booting, also adjusted for the fact that now we are using N-Channel FET to drive the speaker

--- software/geiger/geiger.ino
+++ software/geiger/geiger.ino
@@ -1,34 +1,31 @@
 /**
- * IRF9610 gate (pin 1) connected to OPIN, drain (pin 2) to speaker +, source (pin 3) to +5V (2A supply) and speaker - goes to ground.
+ * IRF740 gate (pin 1) connected to OPIN, drain (pin 2) to speaker -, source (pin 3) to GND  and speaker + goes to supply voltage.
  * Generic potentiometer to analog 1
  * analog 0 is floating to be used as random source.
+ */
+
+/**
+ * Pin notes by Suovula (see also http://hlt.media.mit.edu/?p=1229)
  *
-
-    ATtiny Pin 2 to Arduino Pin 13 (or SCK of another programmer)
-    ATtiny Pin 1 to Arduino Pin 12 (or MISO of another programmer)
-    ATtiny Pin 0 to Arduino Pin 11 (or MOSI of another programmer)
-    ATtiny Reset Pin to Arduino Pin 10 (or RESET of another programmer)
-
+// I2C
+arduino pin 0 = not(OC1A) = PORTB <- _BV(0) = SOIC pin 5 (I2C SDA, PWM)
+arduino pin 2 =           = PORTB <- _BV(2) = SOIC pin 7 (I2C SCL, Analog 1)
+// Timer1 -> PWM
+arduino pin 1 =     OC1A  = PORTB <- _BV(1) = SOIC pin 6 (PWM)
+arduino pin 3 = not(OC1B) = PORTB <- _BV(3) = SOIC pin 2 (Analog 3)
+arduino pin 4 =     OC1B  = PORTB <- _BV(4) = SOIC pin 3 (Analog 2)
  */
 
 #define ANA0 A1
 #define ANA1 A2
-#define OPIN 3
-#define PWMPIN 1
+#define OPIN 1
+#define PWMPIN 0
 #define MIN_ACTIVITITY 15
 // The P-channel needs to be driven this way or there will be a lot of heat (switched to NPN BJT)
 #define DRIVE_HI HIGH
 #define DRIVE_LO LOW
 
 int activity;
-
-void setup()
-{
-    randomSeed(analogRead(ANA0));
-    pinMode(PWMPIN, OUTPUT);
-    pinMode(OPIN, OUTPUT);
-    digitalWrite(OPIN, DRIVE_LO);
-}
 
 inline void pulse()
 {
@@ -37,6 +34,26 @@ inline void pulse()
     delayMicroseconds(300);
     digitalWrite(OPIN, DRIVE_LO);
 }
+
+void setup()
+{
+    // TODO: Tri-state this and wait for input voltage to stabilize 
+    pinMode(3, OUTPUT); // OC1B-, Arduino pin 3, ADC
+    digitalWrite(3, LOW);
+
+
+    pinMode(ANA0, INPUT);
+    pinMode(ANA1, INPUT);
+    randomSeed(analogRead(ANA0));
+    pinMode(PWMPIN, OUTPUT);
+    pinMode(OPIN, OUTPUT);
+
+    pulse();
+
+
+    digitalWrite(3, HIGH);
+}
+
 
 inline int activity_adj(int activity)
 {
