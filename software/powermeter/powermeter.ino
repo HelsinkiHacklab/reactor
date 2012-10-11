3ae5ece93a6aed698af1b8451590d1c0d282f6c6
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-10-11 21:50:54 +0300

    helper functions to get the current sensor outputs to something sensible

--- software/powermeter/powermeter.ino
+++ software/powermeter/powermeter.ino
@@ -4,7 +4,52 @@
 LiquidCrystalFast lcd(RS, RW, Enable, D4, D5, D6, D7) 
 */
 
+#define VCC_MV (5200) // I'm using 5.2V supply
+#define MV_PER_LSB ((uint8_t)(VCC_MV/1024)) // I sure hope the complier will optimize this result to a constant
+#define ACS715_ZERO ((uint16_t)(VCC_MV/10))
+#define ACS714_ZERO ((uint16_t)(VCC_MV/2))
+
+
 LiquidCrystalFast lcd(0, 1, 2, 7, 8, 9, 10);
+
+uint16_t acs715_mv2ma(uint16_t mv)
+{
+    // 133 mV/A starting at 500 mV (actually vcc/100), 1.5% error
+    if (mv <= ACS715_ZERO)
+    {
+        return 0;
+    }
+    mv = mv - ACS715_ZERO; // Zero
+    uint8_t amps = mv / 133;
+    uint8_t desiamps = (mv - (amps*133)) / 13;
+    uint8_t centiamps = (mv - (amps*133) - (desiamps*13));
+    return (amps*1000) + (desiamps * 100) + (centiamps * 10);
+}
+
+inline int8_t acs714_mv2ma_sign(uint16_t mv)
+{
+    if (mv < ACS714_ZERO)
+    {
+        return -1;
+    }
+    return 1;
+}
+
+uint16_t acs714_mv2ma(uint16_t mv)
+{
+    // TODO: check if the 2500mv center point applies on 5.2V supply
+    // 66mV/A centered on 2500mv, 1.5% error
+    
+    // For now I'll just care about the positive side.
+    if (mv <= ACS714_ZERO)
+    {
+        return 0;
+    }
+    mv = mv - ACS714_ZERO;
+    uint8_t amps = mv / 66;
+    uint8_t desiamps = (mv - (amps*66)) / 6;
+    return (amps*1000) + (desiamps * 100);
+}
 
 uint16_t pa4;
 uint16_t pa5;
