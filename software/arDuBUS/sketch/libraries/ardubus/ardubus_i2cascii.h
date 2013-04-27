054e0bf12f83af02c846f7c96763e74f217b9794
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-04-27 09:50:39 +0300

    we need to pass the address to make the pointer work correctly

--- software/arDuBUS/sketch/libraries/ardubus/ardubus_i2cascii.h
+++ software/arDuBUS/sketch/libraries/ardubus/ardubus_i2cascii.h
@@ -34,7 +34,7 @@ inline void ardubus_i2cascii_process_command(char *incoming_command)
         case 0x77: // ASCII "w" (w<index><ascii>...<ascii>) //The index of ardubus_i2cascii_boards
             uint8_t addr = ardubus_i2cascii_boards[incoming_command[1]-ARDUBUS_INDEX_OFFSET];
             // This gives compiler error for some reason error:   initializing argument 2 of 'char* strncpy(char*, const char*, size_t)' [-fpermissive]
-            strncpy(ardubus_i2cascii_buffer, incoming_command[2], ARDUBUS_I2CASCII_BUFFER_SIZE);
+            strncpy(ardubus_i2cascii_buffer, &incoming_command[2], ARDUBUS_I2CASCII_BUFFER_SIZE);
             
             // Write the buffer
             I2c.write(addr, 0x0, (uint8_t*)ardubus_i2cascii_buffer, (uint8_t)strlen(ardubus_i2cascii_buffer));
