a1aca190bcf988f54e159f42b2db0c6f29dcc707
Taneli Kaivola <taneli.kaivola@github.com>
2013-05-18 16:25:34 +0300

    Extremely simple pubsub-test for zmq playground

--- software/pyzqmq_playgound/pub_test.py
+++ software/pyzqmq_playgound/pub_test.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 import zmq
+import itertools
+import random
 
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5555")
+
+topic = itertools.cycle(('test','foo','bar'))
+while True:
+    data = "%s" % random.randint(0,100000)
+    socket.send("%s %s" % (topic.next(), data))
--- software/pyzqmq_playgound/sub_test.py
+++ software/pyzqmq_playgound/sub_test.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 import zmq
 
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://localhost:5555")
+
+socket.setsockopt(zmq.SUBSCRIBE, "test") # topic
+
+while True:
+    recv = socket.recv()
+    topic, data = recv.split(' ',1)
+    print "received %s: %s" % (topic, data)
