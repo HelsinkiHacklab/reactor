46ddb89399dbcdc6debb350ed109b774199394a5
Eero af Heurlin <rambo@iki.fi>
2013-05-18 22:54:20 +0300

    tuned settings for raspi and the BW monitor in the coms-console

--- software/pyzqmq_playgound/gst_test.py
+++ software/pyzqmq_playgound/gst_test.py
@@ -6,10 +6,22 @@ import pygst
 pygst.require("0.10")
 import gst
 
+# These are from /boot/config.txt
+overscan_left=30
+overscan_right=10
+overscan_top=0
+overscan_bottom=0
+
+width=640-overscan_left-overscan_right
+height=420-overscan_top-overscan_bottom
+
+
 class GTK_Main():
 
     def __init__(self):
-        self.player = gst.parse_launch('videotestsrc name=source ! video/x-raw-yuv,format=(fourcc)AYUV,width=500,height=400 ! videomixer name=mix ! ffmpegcolorspace !  autovideosink name=videosink')
+        launch_str = 'videotestsrc name=source ! video/x-raw-yuv,format=(fourcc)AYUV,width=%d,height=%d ! videomixer name=mix ! ffmpegcolorspace !  fbdevsink name=videosink' % (width, height)
+        print "Calling gst.parse_launch('%s')" % launch_str
+        self.player = gst.parse_launch(launch_str)
         self.player.set_state(gst.STATE_PLAYING)
 
 
