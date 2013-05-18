9670c173f17da213674d65cd30f134555b6be8c0
Eero af Heurlin <rambo@iki.fi>
2013-05-18 18:29:56 +0300

    pybonjour is available via pip

--- software/install_dependencies.sh
+++ software/install_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
 # Python and python libs
-sudo apt-get install python python-pygame python-yaml python-zmq python-tornado libavahi-compat-libdnssd1 python-gobject python-pyside python-gst0.10
+sudo apt-get install python python-pip python-pygame python-yaml python-zmq python-tornado libavahi-compat-libdnssd1 python-gobject python-pyside python-gst0.10
+sudo pip install pybonjour
 # Realpath required for calling the launchers via desktop links
 sudo apt-get install realpath
 
