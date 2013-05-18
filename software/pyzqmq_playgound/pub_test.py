c8fe1830ae50de6a816290cf91b346ff3421ad13
Juha Autero <jautero@iki.fi>
2013-05-18 17:36:40 +0300

    Cleaned up conflict.

--- software/pyzqmq_playgound/pub_test.py
+++ software/pyzqmq_playgound/pub_test.py
@@ -27,14 +27,6 @@ sdRef=pybonjour.DNSServiceRegister(service_name,service_type,service_port,bonjou
 
 
 topic = itertools.cycle(('test','foo','bar'))
-<<<<<<< HEAD:software/pyzqmq_playgound/pub_test.py
-while True:
-	ready=socket.socket([sdRef],[],[])
-	if sdRef in ready[0]:
-		pybonjour.DNSServiceProcessResult(sdRef)
-    data = "%s" % random.randint(0,100000)
-    socket.send("%s %s" % (topic.next(), data))
-=======
 
 stream = ZMQStream(socket)
 
@@ -47,4 +39,3 @@ pcb = ioloop.PeriodicCallback(send_random_data, 100)
 pcb.start()
 
 ioloop.IOLoop.instance().start()
->>>>>>> aba07dba85bf51dfc42a3f4de82290dd9d01099f:software/pyzqmq_playgound/pub_test.py
