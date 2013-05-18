aba07dba85bf51dfc42a3f4de82290dd9d01099f
Taneli Kaivola <dist+github@desu.fi>
2013-05-18 17:24:06 +0300

    Added some example data and topic handling

--- software/pyzqmq_playgound/pub_test.py
+++ software/pyzqmq_playgound/pub_test.py
@@ -17,7 +17,7 @@ stream = ZMQStream(socket)
 
 
 def send_random_data():
-    data = "%s" % random.randint(0,100000)
+    data = "%s bottles of beer on the wall" % random.randint(0,100000)
     stream.send_multipart((topic.next(), data))
 
 pcb = ioloop.PeriodicCallback(send_random_data, 100)
