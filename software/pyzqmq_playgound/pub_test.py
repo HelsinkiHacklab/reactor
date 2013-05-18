264fc60be8b495b9a44419efa38615248f9ad62f
Eero af Heurlin <rambo@iki.fi>
2013-05-18 17:14:05 +0300

    use ioloop for timing the sends

--- software/pyzqmq_playgound/pub_test.py
+++ software/pyzqmq_playgound/pub_test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import zmq
+from zmq.eventloop import ioloop
+from zmq.eventloop.zmqstream import ZMQStream
+ioloop.install()
+
 import itertools
 import random
 
@@ -8,6 +12,15 @@ socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5555")
 
 topic = itertools.cycle(('test','foo','bar'))
-while True:
+
+stream = ZMQStream(socket)
+
+
+def send_random_data():
     data = "%s" % random.randint(0,100000)
-    socket.send_multipart((topic.next(), data))
+    stream.send_multipart((topic.next(), data))
+
+pcb = ioloop.PeriodicCallback(send_random_data, 100)
+pcb.start()
+
+ioloop.IOLoop.instance().start()
