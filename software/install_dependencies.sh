59674a147f8dcee0d14c6fe59e2107dd3ea0d942
Eero af Heurlin <rambo@iki.fi>
2013-05-18 17:34:20 +0300

    pybonjour needs this library

--- software/install_dependencies.sh
+++ software/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Python and python libs
-sudo apt-get install python python-pygame python-yaml python-zmq python-tornado python-gobject python-pyside python-gst0.10
+sudo apt-get install python python-pygame python-yaml python-zmq python-tornado libavahi-compat-libdnssd1 python-gobject python-pyside python-gst0.10
 # Realpath required for calling the launchers via desktop links
 sudo apt-get install realpath
 
