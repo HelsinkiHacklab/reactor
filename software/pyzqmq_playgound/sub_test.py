e4784af9e86e50514b775c92e12111418a4e3fc4
Eero af Heurlin <rambo@iki.fi>
2013-05-18 16:36:25 +0300

    Tornano eventloop based subscribe receiver

--- software/pyzqmq_playgound/sub_test.py
+++ software/pyzqmq_playgound/sub_test.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 import zmq
+from zmq.eventloop import ioloop
+from zmq.eventloop.zmqstream import ZMQStream
+ioloop.install()
+
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5555")
-
 socket.setsockopt(zmq.SUBSCRIBE, "test") # topic
 
-while True:
-    recv = socket.recv()
-    topic, data = recv.split(' ',1)
-    print "received %s: %s" % (topic, data)
+stream = ZMQStream(socket)
+
+def rec_callback(msg):
+    print "received %s" % repr(msg)
+
+
+stream.on_recv(rec_callback)
+
+ioloop.IOLoop.instance().start()
+
