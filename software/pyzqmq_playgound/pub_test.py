5dde5a684929bec6e2a9cc546f0e58fa2f4a139d
Taneli Kaivola <dist+github@desu.fi>
2013-05-18 19:12:56 +0300

    Fixed pub_test bonjour stuff so that it works

--- software/pyzqmq_playgound/pub_test.py
+++ software/pyzqmq_playgound/pub_test.py
@@ -7,20 +7,21 @@ ioloop.install()
 import itertools
 import random
 import pybonjour,socket
+from functools import partial
 
-service_type="_zmqpubsub._tcp"
+service_type="_zmqpubsub._tcp."
 service_name="test_pubsub"
 service_port=5555
 
-def bonjour_register_callback(sdRef,errorCode,name,regtype,domain):
+def bonjour_register_callback(sdRef, flags, errorCode, name, regtype, domain):
     if errorCode == pybonjour.kDNSServiceErr_NoError:
         if name!=service_name or regtype != service_type:
             print "Something went wrong. service name or type don't match:",name,regtype
     else:
         print "Error",errorCode
 
-def bonjour_process_callback(fd,events):
-    pybonjour.DNSServiceProcessResult(fd)
+def bonjour_process_callback(fd, events, sdRef=None):
+    pybonjour.DNSServiceProcessResult(sdRef)
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
@@ -43,5 +44,6 @@ pcb.start()
 
 io_loop=ioloop.IOLoop.instance()
 
-io_loop.add_handler(sdRef,bonjour_process_callback,io_loop.READ)
+bonjour_process_callback_sdref = partial(bonjour_process_callback, sdRef = sdRef)
+io_loop.add_handler(sdRef.fileno(),bonjour_process_callback_sdref,io_loop.READ)
 io_loop.start()
