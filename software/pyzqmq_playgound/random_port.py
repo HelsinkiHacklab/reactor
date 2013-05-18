9fdb65807a24209a85ed89e332077c965116966d
Eero af Heurlin <rambo@iki.fi>
2013-05-19 02:03:21 +0300

    move the bonjour service reg to helper module

--- software/pyzqmq_playgound/random_port.py
+++ software/pyzqmq_playgound/random_port.py
@@ -7,35 +7,24 @@ ioloop.install()
 import itertools
 import random
 
-import pybonjour
-from functools import partial
+
+import sys, os
+libs_dir = os.path.join(os.path.dirname( os.path.realpath( __file__ ) ),  '..', 'pythonlibs')
+if os.path.isdir(libs_dir):                                       
+    sys.path.append(libs_dir)
+import bonjour_utilities
+
 
 service_type="_zmqpubsub._tcp."
 service_name="test_pubsub"
-
-def bonjour_register_callback(sdRef, flags, errorCode, name, regtype, domain):
-    if errorCode == pybonjour.kDNSServiceErr_NoError:
-        print "Registered service", name,regtype,domain
-    else:
-        print "Error",errorCode
-
-
-def bonjour_process_callback(fd, events, sdRef=None):
-    pybonjour.DNSServiceProcessResult(sdRef)
-
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 port_selected = socket.bind_to_random_port('tcp://*', min_port=49152, max_port=65535, max_tries=100)
 print "Bound to %s" % repr(port_selected)
 
-sdRef=pybonjour.DNSServiceRegister(name=service_name, regtype=service_type, port=port_selected, callBack=bonjour_register_callback)
-
-
 topic = itertools.cycle(('test','foo','bar'))
-
 stream = ZMQStream(socket)
-
 
 def send_random_data():
     data = "%s bottles of beer on the wall" % random.randint(0,100000)
@@ -46,6 +35,6 @@ pcb.start()
 
 io_loop=ioloop.IOLoop.instance()
 
-bonjour_process_callback_sdref = partial(bonjour_process_callback, sdRef = sdRef)
-io_loop.add_handler(sdRef.fileno(),bonjour_process_callback_sdref,io_loop.READ)
+bonjour_utilities.register_ioloop(io_loop, service_type, service_name, port_selected)
+
 io_loop.start()
