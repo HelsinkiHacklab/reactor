0c4ac7a820d02bfad1482c5519937a8642670668
Eero af Heurlin <rambo@iki.fi>
2013-05-19 14:19:17 +0300

    server gets requests now

--- software/pyzqmq_playgound/async_client.py
+++ software/pyzqmq_playgound/async_client.py
@@ -3,8 +3,39 @@ import zmq
 from zmq.eventloop import ioloop
 from zmq.eventloop.zmqstream import ZMQStream
 ioloop.install()
+import itertools
+import random
 
-# Insert code here
+
+service_type="_zmqdealerrouter._tcp."
+service_name="test_asyncrpc"
+service_port=5556
+
+
+context = zmq.Context()
+socket = context.socket(zmq.DEALER)
+socket.connect("tcp://*:%d"%service_port)
+stream = ZMQStream(socket)
+
+
+def recv_callback(client_id, command, *args):
+    print "recv_callback got %s" % repr(args)
+    if command == "gimme":
+        bottles = args[0]
+        stream.send_multipart((client_id, "Here's %d bottles of beer" % bottles))
+    else:
+        stream.send_multipart([client_id, ] + list(args))
+    
+
+
+stream.on_recv(recv_callback)
+
+def send_random_data():
+    data = "%s" % random.randint(0,100000)
+    stream.send_multipart(("gimme", data))
+
+pcb = ioloop.PeriodicCallback(send_random_data, 100)
+pcb.start()
 
 
 print "starting ioloop"
--- software/pyzqmq_playgound/async_server.py
+++ software/pyzqmq_playgound/async_server.py
@@ -4,7 +4,21 @@ from zmq.eventloop import ioloop
 from zmq.eventloop.zmqstream import ZMQStream
 ioloop.install()
 
-# Insert code here
+service_type="_zmqdealerrouter._tcp."
+service_name="test_asyncrpc"
+service_port=5556
+
+
+context = zmq.Context()
+socket = context.socket(zmq.ROUTER)
+socket.bind("tcp://*:%d"%service_port)
+stream = ZMQStream(socket)
+
+def server_recv_callback(*args):
+    print "recv_callback got %s" % repr(args)
+
+
+stream.on_recv(server_recv_callback)
 
 
 print "starting ioloop"
